cadb781213ae1345af4cb7b7ef94edd3c7a28083
Marrocco M4 <marrocco.simone00@gmail.com>
2026-09-21 14:28:00 +0200

    Raise the version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.simone.watranscriber"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         ndk {
             abiFilters += "arm64-v8a"
         }
